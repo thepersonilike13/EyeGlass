5806f5d8a9e07516080b9b6f5ec4def37892d5f3
Kausik D <kausikdevanathan@gmail.com>
2024-03-31 22:31:30 +0530

    yolo_runner is working for ASL
    The yolo_runner.py is able to detect my sign language.

--- Yolo_Object_Detection/yolo_runner.py
+++ Yolo_Object_Detection/yolo_runner.py
@@ -3,14 +3,16 @@ import cv2
 import time
 
 
-model = YOLO('yolov8m.pt')
+model = YOLO("../models/best.pt")
 
 # Make this run for only 20 seconds
 def runner(timer):
     start = time.time()
     while time.time()< start + timer:
-        results = model.predict(source='0',show=True)
+        model.predict(source='0',show=True)
     cv2.destroyAllWindows()
 
 
-runner(10)
+
+
+runner(45)
